56de025ec4b5845061ea8a3effc14aa81240aca8
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-09-25 11:26:03 +0530

    init

---
+++ client/src/components/Todoform.jsx
@@ -0,0 +1,16 @@
+
+
+const Todoform = () =>{
+
+    return (
+
+        <form className="form">
+            <input
+            placeholder="Enter new todo.."
+            className="input" />
+        </form>
+
+    )
+}
+
+export default Todoform;
