e67b570aaa5ac07d43ef0345ffe8fa0ba4f81406
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-09-25 11:24:44 +0530

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Header from './components/Header';
+import Todoform from './components/Todoform';
 
 function App() {
   return (
     <div className="App">
-     Hello
+     <Header />
+     <Todoform/>
     </div>
   );
 }
