abf5f701d8c9317b11deaa4b4f3be318c46df431
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-09-26 11:41:12 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import './App.css';
-import Header from './components/Header';
-import Todoform from './components/Todoform';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-function App() {
-  return (
-    <div className="App">
-     <Header />
-     <Todoform/>
-    </div>
-  );
-}
-
-export default App;
+reportWebVitals();
