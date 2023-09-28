06627dd0cb089325374e9505d70e4634972e9f44
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-09-28 12:28:09 +0530

    Updated

--- server/database/db.js
+++ server/database/db.js
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
  const Connection =() =>{
 
-    const MONGODB_URI = 'mongodb://user:user123@ac-6tbf6lw-shard-00-00.pbrpik4.mongodb.net:27017,ac-6tbf6lw-shard-00-01.pbrpik4.mongodb.net:27017,ac-6tbf6lw-shard-00-02.pbrpik4.mongodb.net:27017/?ssl=true&replicaSet=atlas-24tr8a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=AtlasApp';
+    const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-6tbf6lw-shard-00-00.pbrpik4.mongodb.net:27017,ac-6tbf6lw-shard-00-01.pbrpik4.mongodb.net:27017,ac-6tbf6lw-shard-00-02.pbrpik4.mongodb.net:27017/?ssl=true&replicaSet=atlas-24tr8a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=AtlasApp`;
 
-    mongoose.connect(MONGODB_URI,{useNewUrlPasser: true});
+    mongoose.connect(MONGODB_URI,{useNewUrlparser: true});
 
     mongoose.connection.on('connected',() => {
         console.log('database connected successfully');
     })
     
-    mongoose.connection.on('dissconnected', () => {
+    mongoose.connection.on('disconnected', () => {
         console.log('discunnected');
     })
    
