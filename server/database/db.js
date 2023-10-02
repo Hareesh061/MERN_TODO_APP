d568c60176dd17003e3a74178fca078e48cea907
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-10-02 11:31:35 +0530

    Updated

--- server/database/db.js
+++ server/database/db.js
@@ -11,21 +11,21 @@ const PASSWORD = process.env.DB_PASSWORD;
 
     const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-6tbf6lw-shard-00-00.pbrpik4.mongodb.net:27017,ac-6tbf6lw-shard-00-01.pbrpik4.mongodb.net:27017,ac-6tbf6lw-shard-00-02.pbrpik4.mongodb.net:27017/?ssl=true&replicaSet=atlas-24tr8a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=AtlasApp`;
 
-    mongoose.connect(MONGODB_URI,{useNewUrlparser: true});
+    mongoose.connect(MONGODB_URI,{ useNewUrlParser: true});
 
     mongoose.connection.on('connected',() => {
         console.log('database connected successfully');
-    })
+    });
     
     mongoose.connection.on('disconnected', () => {
         console.log('discunnected');
-    })
+    });
    
     mongoose.connection.on('error', () => {
         console.log('error with connecting database', error.message);
 
-    })
+    });
 
-}
+};
 
 export default Connection;
