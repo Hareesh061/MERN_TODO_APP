a82da543b24159f96b36bc34d34a85f8e3e6c162
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-10-04 11:24:22 +0530

    Updated

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -1,11 +1,21 @@
 
+import { useEffect } from "react";
+import { getAllTodos } from "../redux/actions/index";
+
+import { useDispatch } from 'react-redux';
 
 
-
-
-
+ 
 
 export const Todos = () =>{
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllTodos());
+
+    },[])
+
     return (
         <div>List will added </div>
     ) 
