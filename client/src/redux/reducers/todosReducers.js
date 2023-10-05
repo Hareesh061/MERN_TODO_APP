7ea1223d1f89658b86dc5ff042a846759928c7fc
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-10-05 15:12:17 +0530

    Updated

--- client/src/redux/reducers/todosReducers.js
+++ client/src/redux/reducers/todosReducers.js
@@ -5,6 +5,8 @@ export const todosReducers = (state={},action) =>{
     switch(action.type) {
         case actionTypes.ADDNEW_TODO:
             return [action.payload, ...state]
+        case actionTypes.GETALL_TODO:
+            return action.payload
 
 
         default:
