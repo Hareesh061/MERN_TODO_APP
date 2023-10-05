30042013b3574465326ad0a31160169614ee2a1a
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-10-05 15:13:44 +0530

    Updated

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,7 +1,7 @@
 //API calls
 import axios from 'axios';
 
-import { ADDNEW_TODO } from './type';
+import { ADDNEW_TODO,GETALL_TODO  } from './type';
 
 const API_URL = 'http://localhost:8000';
 
@@ -17,11 +17,11 @@ export const addNewTodo = (data) => async(dispatch) =>{
     } 
 }
 
-export const getAllTodos = () => async (dispatch) =>{
+export const getAllTodos = (data) => async (dispatch) =>{
     try{
         const res = await axios.get(`${API_URL}/todos`,{data});
  
-       // dispatch({type: ADDNEW_TODO, payload: res.data});
+        dispatch({type: ADDNEW_TODO, payload: res.data});
  
      }catch(error){
          console.log('error while calling getAllTodos API',error.message);
