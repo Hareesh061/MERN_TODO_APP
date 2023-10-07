90890f0b92f89a2d8bf2c1212865f8d59597d3fd
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-10-07 11:54:54 +0530

    Updated

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,10 +2,11 @@ import express from "express";
 
 const route = express.Router();
 
-import { addTodo ,getAllTodos, toggleTodoDone } from '../controller/todo-controller.js'
+import { addTodo ,getAllTodos, toggleTodoDone , updateTodo} from '../controller/todo-controller.js'
  
 route.post('/todos', addTodo)
 route.get('/todos',getAllTodos);
 route.get('/todos/:id', toggleTodoDone);
+route.put('/todos/:id', updateTodo);
 
 export default route;
